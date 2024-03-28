19ef342a80dbd416117039477afcb33bc9bfd990
Multyfora <aryuc06@gmail.com>
2024-03-28 18:29:39 +0400

    added items ruby and raw ruby, added item group for rubies

--- src/main/java/net/multyfora/first_tutor/FirstTutor.java
+++ src/main/java/net/multyfora/first_tutor/FirstTutor.java
@@ -2,6 +2,8 @@ package net.multyfora.first_tutor;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.multyfora.first_tutor.item.ModItemGroups;
+import net.multyfora.first_tutor.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +13,7 @@ public class FirstTutor implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
-		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
 	}
 }
