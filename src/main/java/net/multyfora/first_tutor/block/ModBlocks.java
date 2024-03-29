7b0b61e518b625d44553038b0f288fe5d096bbde
Multyfora <aryuc06@gmail.com>
2024-03-30 00:00:12 +0400

    added ruby ores with deepslate, nether and end variants, also added their loottables and minablility

--- src/main/java/net/multyfora/first_tutor/block/ModBlocks.java
+++ src/main/java/net/multyfora/first_tutor/block/ModBlocks.java
@@ -4,12 +4,14 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.multyfora.first_tutor.FirstTutor;
 
 public class ModBlocks {
@@ -18,6 +20,14 @@ public class ModBlocks {
             new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
     public static final Block RAW_RUBY_BLOCK = registerBlock("raw_ruby_block",
             new Block(FabricBlockSettings.copyOf(Blocks.IRON_ORE).sounds(BlockSoundGroup.METAL) ));
+    public static final Block RUBY_ORE = registerBlock("ruby_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5), FabricBlockSettings.copyOf(Blocks.STONE).strength(3f)));
+    public static final Block DEEPSLATE_RUBY_ORE = registerBlock("deepslate_ruby_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5), FabricBlockSettings.copyOf(Blocks.DEEPSLATE).strength(4.5f)));
+    public static final Block NETHER_RUBY_ORE = registerBlock("nether_ruby_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(3, 6), FabricBlockSettings.copyOf(Blocks.NETHERRACK).strength(2f)));
+    public static final Block END_STONE_RUBY_ORE = registerBlock("end_stone_ruby_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(5, 8), FabricBlockSettings.copyOf(Blocks.END_STONE).strength(3f)));
 
 
     private static Block registerBlock(String name, Block block){
